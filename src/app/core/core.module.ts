005642c80957472cd0e973697fd609d4ab269cec
Steve Doggett <artigat1@users.noreply.github.com>
2017-10-21 09:59:25 +0100

    :sparkles: Ensure the core module is loaded once
    Throw an error if it's included more than one time.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, Optional, SkipSelf} from '@angular/core';
 
 import {SharedModule} from '../shared/shared.module';
 import {HeaderBarComponent} from './header-bar/header-bar.component';
@@ -16,4 +16,10 @@ import {HeaderBarComponent} from './header-bar/header-bar.component';
   providers: []
 })
 export class CoreModule {
+  /* make sure CoreModule is imported only by one NgModule the AppModule */
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import only in AppModule');
+    }
+  }
 }
